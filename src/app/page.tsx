ac6dae2620a625f3e801af91ace58f778cc0c940
Everson <igor@iMacdeDigital10.localdomain>
2025-07-31 11:47:49 -0400

    v0.2 mudanca de preco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import CardCurso from "../../componentes/cardCurso";
 import Image from "next/image";
 import { BadgeCheck, ChartNoAxesColumnIncreasing, CirclePlay, GraduationCap, LucideGraduationCap, ShieldCheck, TvMinimal, UsersRound } from "lucide-react";
 import FAQ from "../../componentes/faq";
@@ -13,8 +12,10 @@ export default function Home() {
       {/* hero banner */}
       <div className="bg-[var(--primary)] bg-[url(/bannerSuperior.png)] bg-top py-40">
         <div className="max-w-[var(--largura)] mx-auto px-5">
-          <h1 className="text-white text-center text-5xl/13 font-extrabold">Se especialize em <span className="block text-[#efcf6b]">Marketing e Vendas</span></h1>
-          <p className="text-white text-center mx-auto text-lg mt-5 max-w-[450px]">A plataforma completa para você dominar as habilidades que o mercado exige: do zero ao avançado.</p>
+          <div className="max-w-[500px] mx-auto">
+            <h1 className="text-white text-center text-5xl/13 font-extrabold">Se especialize em <span className="block text-[#efcf6b]">Marketing ,Vendas, Empreendedorismo e Inovação</span></h1>
+            <p className="text-white text-center mx-auto text-lg mt-5 max-w-[450px]">A plataforma completa para você dominar as habilidades que o mercado exige: do zero ao avançado.</p>
+          </div>
           <div className="flex gap-5 justify-center items-center flex-col md:flex-row mt-15">
             <a href="#cursos" className="bg-[var(--amarelo)] rounded-full px-8 py-5 text-md leading-[1] font-bold uppercase">Ver todos os cursos</a>
             <a href="https://painel.digitaleduca.com.vc/checkout" className="border-2 border-white text-white rounded-full px-8 py-5 text-md leading-[1] font-bold uppercase">Começar agora</a>
@@ -100,13 +101,18 @@ export default function Home() {
         {/* section 5 */}
       <div className="max-w-[var(--largura)] mx-auto">
         <div className="h-auto w-[350px] mx-auto rounded-xl bg-[url('/bgpromo.svg'),linear-gradient(to_top,#0D68F9,#6A97FA)] bg-size-[100%_auto] bg-center bg-no-repeat text-white p-8 text-center">
-          <h3 className="text-xl font-bold uppercase mb-3">Assinatura Anual</h3>
+          <h3 className="text-xl font-bold uppercase mb-3">Assinatura Mensal</h3>
           <p className="text-lg leading-[1.2]">Comece hoje e mude sua carreira profissional.</p>
           <div className="flex items-center justify-center gap-2 mt-5">
             <Image src="/badge.svg" alt="" width={50} height={50}/>
-            <button className="rounded-full border-2 text-white leading-[1] font-bold px-4 py-2 uppercase">1 ano de acesso</button>
+            <button className="rounded-full border-2 text-white leading-[1] font-bold px-4 py-2 uppercase">Plano mensal</button>
           </div>
-          <h4 className="text-4xl font-bold mt-4 mb-5">R$350,00</h4>
+          <div className="flex gap-2 justify-center items-center mt-4 mb-2">
+            <p>De</p>
+            <p className="line-through text-xl leading-[1]">R$149,00</p>
+            <p>por</p>
+          </div>
+          <h4 className="text-4xl font-bold mb-5">R$79,90</h4>
           <a href="https://painel.digitaleduca.com.vc/checkout" className="uppercase block bg-[var(--amarelo)] rounded-full font-bold text-black px-8 py-4">Garantir minha vaga</a>
           <p className="text-sm mt-5 flex gap-2 justify-center"><ShieldCheck size={20}/>Segurança de 7 dias para testes </p>
         </div>
